b2bb9333136e816af9e0c3cf177570150a8afb2d
AdelinoJonas <84252481+AdelinoJonas@users.noreply.github.com>
2022-07-07 11:28:32 -0300

    animação dos icones funcionando

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -11,12 +11,14 @@ function Icons() {
     return (
       <div className="icons">
         {/* <Carousel> */}
-          <img className="html" src={html55} alt="Icons language HTML5"/>
-          <img className="css" src={css3} alt="Icons language CSS3"/>
-          <img className="js" src={javaScript} alt="Icons language JAVASCRIPT"/>
-          <img className="node" src={nodejs} alt="Icons language NODE.JS"/>
-          <img className="sql" src={sql} alt="Icons language SQL"/>
-          <img className="react" src={react} alt="Icons language react"/>
+        {/* <div className="image"> */}
+          <img className=" image html" src={html55} alt="icons language HTML5"/>
+          <img className="image css" src={css3} alt="icons language CSS3"/>
+          <img className="image js" src={javaScript} alt="icons language JAVASCRIPT"/>
+          <img className="image node" src={nodejs} alt="icons language NODE.JS"/>
+          <img className="image sql" src={sql} alt="icons language SQL"/>
+          <img className="image react" src={react} alt="icons language react"/>
+        {/* </div> */}
         {/* </Carousel> */}
       </div>
     );
