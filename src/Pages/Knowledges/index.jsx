081e013196847616cdf9b8063f1fafdd5b258206
AdelinoJonas <84252481+AdelinoJonas@users.noreply.github.com>
2022-07-07 13:33:37 -0300

    informações hard skills

--- src/Pages/Knowledges/index.jsx
+++ src/Pages/Knowledges/index.jsx
@@ -5,11 +5,150 @@ function FrontEnd() {
 
   return (
   <div className="containerknowledges">
-    <h1 className="titleSkills">
-      Skills
-
-      
+    <h1 className="title">
+      HardSkills
     </h1>
+    <div className="leftSide">
+      <p className="me">
+      Aqui estão meus principais conhecimentos na área de desenvolvimento:
+      </p> 
+      <section className="hardSkills">
+        <section className='boxSkill'>
+          <span className="titleSkill me">
+            HTML5
+          </span>
+          <section className="boxLevel"><section className="levelbar1"></section></section>
+          
+        </section>
+        <section className='boxSkill'>
+          <span className="titleSkill me">
+            CSS3
+          </span>
+          <section className="boxLevel"><section className="levelbar2"></section></section>
+          
+        </section>
+        <section className='boxSkill'>
+          <span className="titleSkill me">
+            JAVASCRIPT
+          </span>
+          <section className="boxLevel"><section className="levelbar3"></section></section>
+          
+        </section>
+        <section className='boxSkill'>
+          <span className="titleSkill me">
+            NODE.JS
+          </span>
+          <section className="boxLevel"><section className="levelbar4"></section></section>
+          
+        </section>
+        <section className='boxSkill'>
+          <span className="titleSkill me">
+            POSTGRESQL
+          </span>
+          <section className="boxLevel"><section className="levelbar5"></section></section>
+          
+        </section>
+        <section className='boxSkill'>
+          <span className="titleSkill me">
+            REACT.JS
+          </span>
+          <section className="boxLevel"><section className="levelbar6"></section></section>
+          
+        </section>
+        <section className='boxSkill'>
+          <span className="titleSkill me">
+            SASS
+          </span>
+          <section className="boxLevel"><section className="levelbar7"></section></section>
+          
+        </section>
+      </section>
+      <section className="languages">
+        <h1 className="title">
+          Idiomas
+        </h1>
+        <section className="boxlanguage">
+          <span className="titleSkill me">
+            Ingles
+          </span>
+          <section className="containerLevel">
+            <span className="nametitle description">
+              Speaking
+            </span>
+            <section className="boxLevel">
+              <section className="speakbar1"></section>
+            </section>
+            <span className="nametitle description">
+              Listening
+            </span>
+            <section className="boxLevel">
+              <section className="listenbar1"></section>
+            </section>
+            <span className="nametitle description">
+              write
+            </span>
+            <section className="boxLevel">
+              <section className="writebar1"></section>
+            </section>
+          </section>
+        </section>
+        <section className="boxlanguage">
+          <span className="titleSkill me">
+            Espanhol
+          </span>
+          <section className="containerLevel">
+            <span className="nametitle description">
+              Speaking
+            </span>
+            <section className="boxLevel">
+              <section className="speakbar1"></section>
+            </section>
+            <span className="nametitle description">
+              Listening
+            </span>
+            <section className="boxLevel">
+              <section className="listenbar1"></section>
+            </section>
+            <span className="nametitle description">
+              write
+            </span>
+            <section className="boxLevel">
+              <section className="writebar1"></section>
+            </section>
+          </section>
+        </section>
+        <section className="boxlanguage">
+          <span className="titleSkill me">
+            Português
+          </span>
+          <section className="containerLevel">
+            <span className="nametitle description">
+              Speaking
+            </span>
+            <section className="boxLevel">
+              <section className="speakbar1"></section>
+            </section>
+            <span className="nametitle description">
+              Listening
+            </span>
+            <section className="boxLevel">
+              <section className="listenbar1"></section>
+            </section>
+            <span className="nametitle description">
+              write
+            </span>
+            <section className="boxLevel">
+              <section className="writebar1"></section>
+            </section>
+          </section>
+        </section>
+      </section>
+    </div>
+    <aside className="rightSide">
+      <h1 className='title'>
+        SOFTSKILLS
+      </h1>
+    </aside>   
   </div> 
   );
 }
--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -5,21 +5,18 @@ import javaScript from '../../images_all/home/jss.png';
 import nodejs from '../../images_all/home/nodejs.png';
 import sql from '../../images_all/home/SQL.png';
 import react from '../../images_all/home/react.png';
-// import { Carousel } from 'react-responsive-carousel';
+import sass from '../../images_all/home/sass.png';
 
 function Icons() {
     return (
       <div className="icons">
-        {/* <Carousel> */}
-        {/* <div className="image"> */}
-          <img className=" image html" src={html55} alt="icons language HTML5"/>
-          <img className="image css" src={css3} alt="icons language CSS3"/>
-          <img className="image js" src={javaScript} alt="icons language JAVASCRIPT"/>
-          <img className="image node" src={nodejs} alt="icons language NODE.JS"/>
-          <img className="image sql" src={sql} alt="icons language SQL"/>
-          <img className="image react" src={react} alt="icons language react"/>
-        {/* </div> */}
-        {/* </Carousel> */}
+          <img className=" imageIcon html" src={html55} alt="icons language HTML5"/>
+          <img className="imageIcon css" src={css3} alt="icons language CSS3"/>
+          <img className="imageIcon js" src={javaScript} alt="icons language JAVASCRIPT"/>
+          <img className="imageIcon node" src={nodejs} alt="icons language NODE.JS"/>
+          <img className="imageIcon sql" src={sql} alt="icons language SQL"/>
+          <img className="imageIcon react" src={react} alt="icons language react"/>
+          <img className="imageIcon sass" src={sass} alt="icons language react"/>
       </div>
     );
   }
