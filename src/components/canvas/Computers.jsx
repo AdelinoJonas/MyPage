aae0c6639ee39d80ade5119d1496dfa49b7b9712
Jonas Adelino <84252481+AdelinoJonas@users.noreply.github.com>
2023-05-04 19:59:10 -0300

    fix: updating mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -62,7 +62,7 @@ function ComputersCanvas () {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
+        {/* <Computers isMobile={isMobile} /> */}
       </Suspense>
 
       <Preload all />
