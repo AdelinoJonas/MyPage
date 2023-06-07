71bb0704378bf41b2001c0a15ba5717dbcbac083
unknown <jonas.gastro91@gmail.com>
2023-06-07 18:33:43 -0300

    feat: if isMobile computer desapear

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -62,7 +62,7 @@ function ComputersCanvas () {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
+        {!isMobile && <Computers />}
       </Suspense>
 
       <Preload all />
